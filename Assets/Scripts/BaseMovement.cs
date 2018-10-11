bb3dba48eecd7864232d6b6c393767cba911f9cc
Joar <joar.andersson@elev.ga.lbs.se>
2018-10-11 09:08:18 +0200

    Wack movement I did all by myself
    This is all my own work and it was no thanks to William H, bless him

---
+++ Assets/Scripts/BaseMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BaseMovement : MonoBehaviour
+{
+
+    Rigidbody2D rb2d;
+
+    [Header("Speed")]
+    [Range(0.1f, 20f)]
+    public float speed;
+    [Range(0.1f, 10f)]
+    public float turnSpeed;
+    [Range(0.1f, 10f)]
+    public float maxSpeed;
+
+    public float moveVertical;
+    float moveHorizontal;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.gravityScale = 0;
+    }
+
+    void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+
+        moveVertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 movement;
+        movement.x = moveHorizontal * speed;
+        movement.y = moveVertical * speed;
+        //rb2d.velocity = movement;
+        rb2d.AddForce(Vector2.right * speed * moveHorizontal);
+        rb2d.AddForce(Vector2.up * speed * moveVertical);
+
+        if (rb2d.velocity.x > maxSpeed)
+        {
+            Vector2 velocity = rb2d.velocity;
+            velocity.x = maxSpeed;
+            rb2d.velocity = velocity;
+        }
+        else if (rb2d.velocity.x < -maxSpeed)
+        {
+            Vector2 velocity = rb2d.velocity;
+            velocity.x = -maxSpeed;
+            rb2d.velocity = velocity;
+        }
+
+        if (rb2d.velocity.y >= maxSpeed)
+        {
+            Vector2 velocity = rb2d.velocity;
+            velocity.y = maxSpeed;
+            rb2d.velocity = velocity;
+        }
+        else if (rb2d.velocity.y <= -maxSpeed)
+        {
+            Vector2 velocity = rb2d.velocity;
+            velocity.y = -maxSpeed;
+            rb2d.velocity = velocity;
+        }
+    }
+}
